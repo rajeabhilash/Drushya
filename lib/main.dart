1b4fb8e0e40e864172b3b7cc15bc59f60734e976
Raje Abhilash <rajeabhilashmohite@gmail.com>
2022-12-21 23:58:39 +0530

    D1 U3 Basic Widget motion

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
-import 'package:drushya/Widgets/TabAndDrawer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const StartDushya());
+  runApp(const StartDrushya());
 }
 
-class StartDushya extends StatelessWidget {
-  const StartDushya({Key? key}) : super(key: key);
+class StartDrushya extends StatelessWidget {
+  const StartDrushya({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Sarav App",
+    return const MaterialApp(
+      title: "Drushya App",
       debugShowCheckedModeBanner: false,
-      home: TabAndDrawer()
+      home: Scaffold(
+        body: Center(child: Text("Welcome, Drushya Here.!!"),),
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:drushya/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(StartDushya());
+    await tester.pumpWidget(StartDrushya());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
