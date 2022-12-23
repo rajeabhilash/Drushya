f30b91b149346fe4faac198b6c9d117044db7d43
Raje Abhilash Mohite (Chavhan Hambirrao) <rajeabhilashmohite@outlook.com>
2022-12-23 23:52:08 +0530

    D2 U2 Firebase UI Changed Still Need more updates to do on UI Side.

--- lib/Firebase/login.dart
+++ lib/Firebase/login.dart
@@ -15,68 +15,67 @@ class _LoginPageState extends State<LoginPage> {
       body: SafeArea(
         child: Center(
           child: Column(
-            children: <Widget> [
-              SizedBox(height: 20),
-              Text(
+            children: <Widget>[
+              const SizedBox(height: 20),
+              const Text(
                 "Hello Again",
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
               ),
-              SizedBox(height: 10),
-              Text(
+              const SizedBox(height: 10),
+              const Text(
                 'Welcome Back, You\'ve been missed!',
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12.0)
-                  ),
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12.0)),
                   child: Padding(
                     padding: EdgeInsets.symmetric(horizontal: 12),
                     child: TextField(
                       decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Email"
-                      ),
+                          border: InputBorder.none, hintText: "Email"),
                     ),
                   ),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12.0)
-                  ),
-                  child: Padding(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12.0)),
+                  child: const Padding(
                     padding: EdgeInsets.symmetric(horizontal: 12),
                     child: TextField(
                       obscureText: true,
                       decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Password"
-                      ),
+                          border: InputBorder.none, hintText: "Password"),
                     ),
                   ),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 25),
                 child: Container(
                   padding: EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                    color: Colors.deepPurple,
-                    borderRadius: BorderRadius.circular(12)
-                  ),
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(12)),
                   child: Center(
-                    child: Text("Login",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
                   ),
                 ),
               )
