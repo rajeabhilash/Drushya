2722094464abdee630fb148b23d6e0a7bf9bbad1
Raje Abhilash Mohite (Chavhan Hambirrao) <rajeabhilashmohite@outlook.com>
2022-12-25 22:41:40 +0530

    D3 U2 Data Structure Added - API List - JSON List - GridView List are added.
    Will Modify Them Later.

---
+++ lib/DataStructure/APIData.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class APIData extends StatelessWidget {
+  const APIData({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/DataStructure/JSONData.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class JSONData extends StatelessWidget {
+  const JSONData({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
